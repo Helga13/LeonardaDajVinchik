2ba5af6ea4cde06031ed6fa41af71970647a75be
Helga Helen <oliaterubina@yandex.ru>
2018-02-25 21:55:36 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  
+  'use strict'
+  
+  // sliders
+  $('.main_slider').slick({
+    arrows: true,
+//    infinite: true,
+//    autoplay: true,
+    autoplaySpeed: 4000    
+  });
+  
+  // tabs
+  $('.tabs_block').each(function () {
+    $(this).find('.tab').each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $(this).addClass('active').siblings().removeClass('active').closest('.tabs_block').find('.tabs_content').removeClass('active').eq(i).addClass('active');
+      });
+    });
+  });
+  
+  
+});
